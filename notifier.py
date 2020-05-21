52d9022bd25152712439744e6d8e178bc1a69ef8
Alessandro Scisca <ale.scisca2@gmail.com>
2020-05-21 13:10:50 +0200

    Add startup notifier

---
+++ notifier.py
@@ -0,0 +1,28 @@
+import socket
+import urllib.request
+import json
+import pushover
+
+def read_token(ftoken):
+    with open(ftoken, 'r') as f:
+        return f.readline().strip()
+
+
+if __name__ == '__main__':
+    # Acquire information about the device
+    # Public ip
+    ipify_token = read_token('.ipify-token')
+    url = "https://geo.ipify.org/api/v1?apiKey={api_key}".format(api_key=ipify_token)
+    request = urllib.request.urlopen(url).read().decode('utf8')
+    ipdata = json.loads(request)
+    # Local information
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    # Send notification
+    title = "{dev} turned on".format(dev=hostname)
+    message = "{dev} is online and connected with IP {ip_pub}, localized in {city}, {country} ({lat} N, {long} E)." \
+        .format(dev=hostname,ip_pub=ipdata['ip'], city=ipdata['location']['city'],
+                country=ipdata['location']['country'], lat=ipdata['location']['lat'], long=ipdata['location']['lng'])
+
+    po_client = pushover.Client(config_path='.pushoverrc')
+    po_client.send_message(message, title=title, timestamp=True)
